2b79401768e6e0868c117b0e67b52a1343da67b6
RengarajR404 <renga01042002@gmail.com>
2021-07-07 19:25:09 +0530

    commit 1

--- CIA.c
+++ CIA.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 typedef struct
 {
     /* 
@@ -7,25 +8,25 @@ typedef struct
     - age unsigned int
     - list of items purchased would be 2d array of characters or to be simple it's array of strings
     */
-   char mobile_no[10], name[20], gender, date[20][10], purchase_list[20][4];
+   char mobile_no[10], name[20], gender;
    unsigned int age;
+   char date[20][10], purchase_list[20][4]; // dd/mm/yyyy  C101 C102 C103 C104 C105
 }customer;
 
-void add_purchase_detail(customer *);
+void add_purchase_detail(customer [] );
 
 int main(void)
 {
-    customer *cust[10];
-    add_purchase_detail(&cust);
+    customer cust[10];
+    add_purchase_detail(cust);
     return 0;
 }
 
 //adding customer details
 
-void add_purchase_detail(customer *c)
+void add_purchase_detail(customer c[10])
 {
     static int i=5;
-    c->name[0]="Customer 1";
-    printf("%s", c->name[0]);
+    c[0]={"9876543210", "Customer1", 'M', 18, 22/02/1969, "C101"};
     return ;
 }
